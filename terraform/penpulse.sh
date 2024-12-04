d9f8eda19048f8e308b58970b4c1234dfdf22afd
Donald Novasky <dnovasky@users.noreply.github.com>
2024-12-04 13:15:54 -0800

    updated ec2 script

--- terraform/penpulse.sh
+++ terraform/penpulse.sh
@@ -5,6 +5,7 @@ sudo yum -y update
 
 # Install Apache web server
 sudo yum -y install httpd
+sudo yum -y install mod_ssl
 
 # Start Apache web server
 sudo systemctl start httpd.service
@@ -13,5 +14,5 @@ sudo systemctl start httpd.service
 sudo systemctl enable httpd.service
 
 sudo yum install git -y
-git clone https://github.com/sameernagar-hub/PenPulseAWS.git
-sudo mv PenPulseAWS/* /var/www/html/
+git clone https://github.com/dnovasky/PenPulseAWS.git
+sudo mv PenPulseAWS/dist/* /var/www/html/
